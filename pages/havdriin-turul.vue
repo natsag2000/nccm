37bb351caccb904535c04140fc0157c93e7d75e5
Nagi <natsag2000@gmail.com>
2020-05-14 18:42:03 +0200

    Add turul

---
+++ pages/havdriin-turul.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div v-html="buduunShuluunGedesContent" class="mx-4" />
+  </div>
+</template>
+
+<script>
+import buduunShuluunGedes from '~/static/content/Buduun-shuluun-gedes.md'
+export default {
+  computed: {
+    buduunShuluunGedesContent () {
+      return buduunShuluunGedes
+    }
+  }
+}
+</script>
+
+<style>
+  .container {
+    @apply min-h-screen flex flex-col mx-auto;
+  }
+
+</style>
