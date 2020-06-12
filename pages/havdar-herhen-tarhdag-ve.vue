6d7cd2f799695c35177ced01b1d5497c98773ac3
Nagi <natsag2000@gmail.com>
2020-06-12 07:39:02 +0200

    MDs Linked

---
+++ pages/havdar-herhen-tarhdag-ve.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div v-html="pageContent" class="mx-4" />
+  </div>
+</template>
+
+<script>
+import mdContent from '~/static/content/havdar-herhen-tarhdag-ve.md'
+export default {
+  computed: {
+    pageContent () {
+      return mdContent
+    }
+  }
+}
+</script>
+
+<style>
+  .container {
+    @apply min-h-screen flex flex-col mx-auto;
+  }
+
+</style>
