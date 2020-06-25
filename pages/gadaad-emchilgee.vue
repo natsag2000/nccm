784aee5491fad135bdbec5286e71a2df8db8d7f2
Nagi <natsag2000@gmail.com>
2020-06-25 15:43:30 +0200

    Changes

--- pages/gadaad-emchilgee.vue
+++ pages/gadaad-emchilgee.vue
@@ -3,12 +3,11 @@
     <div v-html="pageContent" class="mx-4" />
     <div class="flex ml-4 items-center">
       <a href="/static/gadaad-emchilgeenii-tushaal.pdf" target="_blank">
-        <svg class="h-4 w-4 text-blue-700 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+        <svg class="h-4 w-4 text-blue-700 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
       </a>
       <a class="ml-2 text-blue-700" href="/static/gadaad-emchilgeenii-tushaal.pdf" target="_blank">
         Гадаад эмчилгээний тушаал
       </a>
-
     </div>
   </div>
 </template>
--- pages/havdriin-turul.vue
+++ pages/havdriin-turul.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="container page">
-    <div v-html="buduunShuluunGedesContent" class="mx-4" />
+    <div v-html="buduunShuluunGedesContent" class="mx-4 my-4" />
+    <div v-html="uushgiContent" class="mx-4" />
   </div>
 </template>
 
 <script>
 import buduunShuluunGedes from '~/static/content/Buduun-shuluun-gedes.md'
+import uushgi from '~/static/content/uushgi.md'
 export default {
   computed: {
     buduunShuluunGedesContent () {
       return buduunShuluunGedes
+    },
+    uushgiContent () {
+      return uushgi
     }
   }
 }
