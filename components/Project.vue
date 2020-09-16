bc9420aaddb11840844c66fbe746b6003682534a
Nagi <natsag2000@gmail.com>
2020-09-16 15:30:06 +0200

    Remove Project pages

--- components/Project.vue
+++ components/Project.vue
@@ -13,7 +13,7 @@
         </p>
         <nuxt-link :to="link">
           <div class="flex items-center text-ccm-blue">
-            <p href="project1">
+            <p>
               Дэлгэрэнгүй
             </p>
             <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" /></svg>
