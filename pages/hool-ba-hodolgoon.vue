f0c4c4c077a84daba2012874819d22b193750aaa
Nagi <natsag2000@gmail.com>
2020-10-12 10:47:10 +0200

    Add pictures to pages

--- pages/hool-ba-hodolgoon.vue
+++ pages/hool-ba-hodolgoon.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="container page">
-    <div v-html="pageContent" class="mx-4" />
+    <div class="mx-4 pt-4 my-4">
+      <span class="text-3xl uppercase">Хоол хүнс ба дасгал хөдөлгөөн</span>
+    </div>
+    <div class="flex flex-wrap md:flex-no-wrap">
+      <div v-html="pageContent" class="mx-4 md:w-1/2" />
+      <div class="flex flex-col md:w-1/2">
+        <div class="pt-4">
+          <div class="bg-gray-100 border rounded-lg overflow-hidden">
+            <img class="w-full object-cover" src="img/iluudel_jin.jpg">
+          </div>
+        </div>
+        <div class="pt-4">
+          <div class="bg-gray-100 border rounded-lg overflow-hidden">
+            <img class="w-full object-cover" src="img/targalalt.jpg">
+          </div>
+        </div>
+        <div class="pt-4">
+          <div class="bg-gray-100 border rounded-lg overflow-hidden">
+            <img class="w-full object-cover" src="img/targalalt_2.jpg">
+          </div>
+        </div>
+        <div class="pt-4">
+          <div class="bg-gray-100 border rounded-lg overflow-hidden">
+            <img class="w-full object-cover" src="img/targalalt_3.jpg">
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
--- pages/tamhi.vue
+++ pages/tamhi.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="container page">
-    <div v-html="pageContent" class="mx-4" />
+    <div class="mx-4 pt-4 my-4">
+      <span class="text-3xl uppercase">Тамхи</span>
+    </div>
+    <div class="flex flex-wrap md:flex-no-wrap">
+      <div v-html="pageContent" class="mx-4 md:w-1/2" />
+      <div class="flex flex-col md:w-1/2">
+        <div class="pt-4">
+          <div class="bg-gray-100 border rounded-lg overflow-hidden">
+            <img class="w-full object-cover" src="img/tamhi2.jpg">
+          </div>
+        </div>
+        <div class="pt-4">
+          <div class="bg-gray-100 border rounded-lg overflow-hidden">
+            <img class="w-full object-cover" src="img/tamhi3.jpg">
+          </div>
+        </div>
+        <div class="pt-4">
+          <div class="bg-gray-100 border rounded-lg overflow-hidden">
+            <img class="w-full object-cover" src="img/tamhi.jpg">
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
