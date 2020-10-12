7e20ff8c45a83fb16e0085c151502d3a58fa1492
Nagi <natsag2000@gmail.com>
2020-10-12 11:06:36 +0200

    Add img to ersdliig buuruulah

--- pages/ersdliig-buuruulah.vue
+++ pages/ersdliig-buuruulah.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="container">
-    <div v-html="pageContent" class="mx-4" />
+  <div class="container page">
+    <div class="mx-4 pt-4 my-4">
+      <span class="text-3xl uppercase">Эрсдлийг бууруулах</span>
+    </div>
+    <div class="flex flex-wrap md:flex-no-wrap">
+      <div v-html="pageContent" class="mx-4 md:w-1/2" />
+      <div class="flex flex-col md:w-1/2">
+       <div class="pt-4">
+          <div class="bg-gray-100 border rounded-lg overflow-hidden">
+            <img class="w-full object-cover" src="img/ersdelt_huchin_zuil2.jpg">
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,6 +30,10 @@ export default {
 <style>
   .container {
     @apply min-h-screen flex flex-col mx-auto;
+  }
+  .page ul {
+    list-style-type: circle;
+    list-style-position: inside;
   }
 
 </style>
