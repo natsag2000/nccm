2ca914aa25941c87b9a850843694ace9dfb11fff
Nagi <natsag2000@gmail.com>
2020-10-12 09:54:14 +0200

    Add picture to ceo and hundet zovloh

--- pages/ceo.vue
+++ pages/ceo.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="container">
-    <div v-html="mdContent" class="mx-4" />
+    <div class="mx-4 pt-4 my-4">
+      <span class="text-3xl uppercase">Гүйцэтгэх захирлын мэндчилгээ</span>
+    </div>
+    <div class="flex flex-wrap md:flex-no-wrap">
+      <div class="pt-4">
+        <div class="bg-gray-100 border rounded-lg overflow-hidden">
+          <img class="w-full object-cover" src="img/tsetsegsaikhan_photo.jpg">
+          <div class="p-2 flex flex-col items-end">
+            <span class="text-sm text-gray-700">Хүний их эмч, анагаах ухааны доктор</span>
+            <span class="text-sm text-gray-700 uppercase">Б.Цэцэгсайхан</span>
+          </div>
+        </div>
+      </div>
+      <div v-html="mdContent" class="mx-4" />
+    </div>
   </div>
 </template>
 
--- pages/pageHundetZovloh.vue
+++ pages/pageHundetZovloh.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="container">
-    <div v-html="pageContent" class="mx-4" />
+    <div class="mx-4 pt-4 my-4">
+      <span class="text-3xl uppercase">Хүндэт зөвлөх</span>
+    </div>
+    <div class="flex flex-wrap md:flex-no-wrap">
+      <div class="pt-4 md:w-1/3">
+        <div class="bg-gray-100 border rounded-lg overflow-hidden">
+          <img class="w-full object-cover" src="img/hundet_zovloh.jpeg">
+          <div class="p-2 flex flex-col items-end">
+            <span class="text-sm text-gray-700">Хүндэт зөвлөх</span>
+            <span class="text-sm text-gray-700 uppercase">Жон Лангтри</span>
+          </div>
+        </div>
+      </div>
+      <div v-html="pageContent" class="mx-4 md:w-2/3" />
+    </div>
+
   </div>
 </template>
 
