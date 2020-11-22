0a05bc8eb3e64eec740518004bf3efcae2ab78ea
Nagi <natsag2000@gmail.com>
2020-11-22 19:34:20 +0100

    Split donation-page

---
+++ pages/zardal-zartsuulalt.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container page">
+    <div v-html="pageContent" class="mx-4" />
+  </div>
+</template>
+
+<script>
+
+export default {
+  computed: {
+    pageContent () {
+      return this.mdContent
+    }
+  },
+  async asyncData ({ $axios }) {
+    const url = 'https://raw.githubusercontent.com/natsag2000/nccm/master/static/content/zardal-zartsuulalt.md'
+    const md = require('markdown-it')()
+      .use(require('markdown-it-multimd-table'))
+    const { data } = await $axios.get(url)
+    return {
+      mdContent: md.render(data)
+    }
+  }
+}
+</script>
+
+<style>
+  .container {
+    @apply min-h-screen flex flex-col mx-auto;
+  }
+
+  .page ul {
+    list-style-type: circle;
+    list-style-position: inside;
+  }
+</style>
